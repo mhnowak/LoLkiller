1073e29a92e4479ba565a8b3726ac582a4ba01aa
Michal Nowak <mhnowakk@gmail.com>
2020-11-03 18:05:39 +0100

    Don't allow for multiple instances of the app to run

--- LoLkiller/Program.cs
+++ LoLkiller/Program.cs
@@ -1,11 +1,18 @@
-﻿using System.Windows.Forms;
+﻿using System.Threading;
+using System.Windows.Forms;
 
 namespace LoLkiller
 {
     static class Program
     {
+        private static Mutex mutex = null;
+
         static void Main()
         {
+            mutex = new Mutex(true, Application.ProductName, out bool createdNew);
+
+            if (!createdNew) return;
+
             new LoLKillHook();
             Form form = new Form1();
             Application.Run();
